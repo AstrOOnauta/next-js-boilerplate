fe4bff87af89c3bb353a3724d1737b7a0eedede5
Will <willian.ralf.rn@gmail.com>
2023-06-11 13:16:37 -0300

    feat: create npx tool

---
+++ bin/cli.js
@@ -0,0 +1,33 @@
+#!/usr/bin/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (error) {
+    console.error(`Failed to execute ${command}`, error);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/AstrOOnauta/next-13-boilerplate.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+
+const checkedOut = runCommand(gitCheckoutCommand);
+
+if (!checkedOut) process.exit(code - 1);
+
+console.log(`Installing dependencies for ${repoName}`);
+
+const installDeps = runCommand(installDepsCommand);
+
+if (!installDeps) process.exit(code - 1);
+
+console.log("Gratz! You're ready. Follow the following commands to start:");
+console.log(`cd ${repoName} && npm run dev`);
